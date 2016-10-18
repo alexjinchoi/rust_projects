535a48e55cd4bb3066b9fa4717a80bdc0a15920f
Alex Jin Choi <alexjinchoi@gmail.com>
2016-10-18 19:43:16 +0900

    add comparing guess

--- rust-projects/guessing_game/src/main.rs
+++ rust-projects/guessing_game/src/main.rs
@@ -1,13 +1,14 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    
+
     println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
@@ -18,4 +19,10 @@ fn main() {
         .expect("failed to read line");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
 }
