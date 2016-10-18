a185b99da541b3a634bd031327fbea80f06119e7
Alex Jin Choi <alexjinchoi@gmail.com>
2016-10-18 16:49:34 +0900

    add dependencies rand="0.3.0"

---
+++ rust-projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\KAIS\git\rust-projects\rust-projects\guessing_game\target\debug\guessing_game.exe: src\main.rs
+
+C:\Users\KAIS\git\rust-projects\rust-projects\guessing_game\target\debug\guessing_game.d: src\main.rs
+
+src\main.rs:
