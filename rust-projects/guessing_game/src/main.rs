08a2303f53bdd59d309b6072a40e7a6a05dca0e5
Alex Jin Choi <alexjinchoi@gmail.com>
2016-10-18 18:29:22 +0900

    delete expect()

--- rust-projects/guessing_game/src/main.rs
+++ rust-projects/guessing_game/src/main.rs
@@ -14,8 +14,7 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("failed to read line");
+    io::stdin().read_line(&mut guess);
 
     println!("You guessed: {}", guess);
 }
