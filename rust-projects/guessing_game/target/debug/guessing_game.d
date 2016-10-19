7355d03a2d6b650ee2eab4a3dc8308c78942d105
Alex Jin Choi <alexjinchoi@gmail.com>
2016-10-19 20:30:22 +0900

    added looping

--- rust-projects/guessing_game/target/debug/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\KAIS\git\rust-projects\rust-projects\guessing_game\target\debug\guessing_game.exe: src\main.rs
-
-C:\Users\KAIS\git\rust-projects\rust-projects\guessing_game\target\debug\guessing_game.d: src\main.rs
-
-src\main.rs:
